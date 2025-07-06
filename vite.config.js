507132f0ffd15fb0036406f32c51b8e3cc8adba6
nohemy morelo <nohemy_morelo69222@elpoli.edu.co>
2025-07-05 22:10:06 -0500

    Configura rutas para despliegue en GitHub Pages y corrige index.html intento 4

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Galeria-dibujos/', // nombre exacto del repo
+  base: '/GaleriaDeArte/', 
   plugins: [react()],
 })
