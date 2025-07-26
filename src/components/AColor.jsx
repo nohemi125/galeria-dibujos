4aef33cb45530188bed5d3ec323028bc087baaca
nohemy morelo <nohemy_morelo69222@elpoli.edu.co>
2025-07-26 14:34:36 -0500

    corregir error de imagen mal importda

--- src/components/AColor.jsx
+++ src/components/AColor.jsx
@@ -10,7 +10,7 @@ import dibujo6 from "../assets/acolor/dibujo6.JPG"
 import dibujo7 from "../assets/acolor/dibujo7.JPG"
 import dibujo8 from "../assets/acolor/dibujo8.JPG"
 import dibujo9 from "../assets/acolor/dibujo9.JPG"
-import dibujo10 from "../assets/acolor/dibujo10.JPG"
+import dibujo10 from "../assets/acolor/dibujo10.jpg"
 
 
 import { useState } from "react"
